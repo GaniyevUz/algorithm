13a1d69ba80f50990501b3a325ca195646b51184
Jahongir <ganiyevjahongir03@gmail.com>
2022-10-28 22:12:53 +0500

    added 20. Valid Parentheses

---
+++ leetcode.com/valid-parentheses.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/valid-parentheses/
+
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#
+# An input string is valid if:
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+
+# Input: s = "()[]{}"
+# Output: true
+#
+# Input: s = "(]"
+# Output: false
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('()', '')
+            s = s.replace('{}', '')
+            s = s.replace('[]', '')
+            #  or one line
+            # s = s.replace('()', '').replace('{}', '').replace('[]', '')
+
+        return len(s) == 0
