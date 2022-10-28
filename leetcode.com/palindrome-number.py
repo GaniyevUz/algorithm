5b932694f4c655428d665d4eb3c51791e35f8781
Jahongir <ganiyevjahongir03@gmail.com>
2022-10-28 18:52:56 +0500

    solved with 2 pointers

--- leetcode.com/palindrome-number.py
+++ leetcode.com/palindrome-number.py
@@ -12,7 +12,15 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return True if str(x)[::-1] == str(x) else False
+        x = str(x)
+        l, r = 0, len(x) - 1
+        while l < r:
+            if x[l] != x[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+        # return True if str(x)[::-1] == str(x) else False
 
 
 x = 121
